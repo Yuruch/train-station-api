0a1bfddce93723ab8fa03cd5737fc4a5cc4efe3d
Yuruch <fuceqwertyt@gmail.com>
2024-08-08 10:15:37 +0300

    fix settings

--- train_station_api/settings.py
+++ train_station_api/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "train_station",
-    "rest_framework" "user",
+    "user",
 ]
 
 MIDDLEWARE = [
